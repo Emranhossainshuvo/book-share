6be5df9b87fba389aedf143b501d4b313f09008a
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-30 12:35:36 +0600

    data store in the mongodb through backend functionality added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Providers/AuthProvider";
 
 const Navbar = () => {
-
+    
     const { user } = useContext(AuthContext)
     const [menuOpen, setMenuOpen] = useState(false);
 
