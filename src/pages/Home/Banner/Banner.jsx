bd59d8bf17d93b04593b6fed0316c7fbca6b0692
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-30 22:14:33 +0600

    changed the banner section style

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,8 +2,9 @@
 const Banner = () => {
   return (
     <>
-      <section className="bg-cover h-[80vh] bg-black " style={{ backgroundImage: "url(https://i.ibb.co/s2Jp4rp/4301.jpg)" }}>
-        <div className="flex max-w-screen-2xl mx-auto justify-between items-center">
+      <section className="bg-cover relative bg-center h-[80vh]" style={{ backgroundImage: "url(https://i.ibb.co/s2Jp4rp/4301.jpg)" }}>
+        <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
+        <div className="flex relative  bg-opacity-50 max-w-screen-2xl h-full mx-auto justify-between items-center">
           <div>
             <h4 className="text-white text-4xl font-semibold">Start Sharing Today!</h4>
           </div>
