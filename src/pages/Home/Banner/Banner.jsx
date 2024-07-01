ae03175af2c4218d1b2c61d2c8a386408e0d15f2
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-01 08:27:37 +0600

    banner background styles added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -2,7 +2,7 @@
 const Banner = () => {
   return (
     <>
-      <section className="bg-cover relative bg-center h-[80vh]" style={{ backgroundImage: "url(https://i.ibb.co/s2Jp4rp/4301.jpg)" }}>
+      <section className="bg-cover bg-fixed relative bg-center h-[80vh]" style={{ backgroundImage: "url(https://i.ibb.co/s2Jp4rp/4301.jpg)" }}>
         <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
         <div className="flex relative  bg-opacity-50 max-w-screen-2xl h-full mx-auto justify-between items-center">
           <div>
